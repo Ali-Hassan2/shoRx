c7000f4fd6424eb78062d331d76015774bb61368
Ali-Hassan2 <alihassan26032004@gmail.com>
2025-08-23 15:32:12 +0500

    working on logout

--- alito/ui/admin/dashboard/dashboard.tsx
+++ alito/ui/admin/dashboard/dashboard.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { adminlogout } from "./actions/admin";
+import { showError } from "@/Utils";
 import { useRouter } from "next/navigation";
 
 const Dashboard = () => {
@@ -6,11 +8,18 @@ const Dashboard = () => {
   const handlerouting = (path: string): void => {
     router.push(path);
   };
-  const handlelogout = () => {
-    const token = localStorage.getItem("admintoken");
-    if (token) {
-      localStorage.removeItem("admintoken");
-      handlerouting("/admin/signup");
+  const handlelogout = async () => {
+    try {
+      const result = await adminlogout();
+      if (result.success) {
+        showError("Logout Successfull.");
+        handlerouting("/admin/login");
+      } else {
+        showError(`Cannot logout. ${result?.message}`);
+      }
+    } catch (error: unknown) {
+      console.log("Unexpected error.", error);
+      showError("Something went wrong. Please try again.s");
     }
   };
 
