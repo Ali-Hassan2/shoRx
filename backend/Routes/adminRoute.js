4703e712c0dd0888bc751c74a378060edadc1438
Ali-Hassan2 <alihassan26032004@gmail.com>
2025-08-23 15:03:34 +0500

    working on logout

--- backend/Routes/adminRoute.js
+++ backend/Routes/adminRoute.js
@@ -3,10 +3,12 @@ const adminMiddleware = require("../Middlewares/adminMiddleware");
 const {
   creatingadmin,
   proceedingadmin,
+  adminlogout,
 } = require("../Controller/adminController");
 const router = express.Router();
 
 router.post("/signup", adminMiddleware, creatingadmin);
+router.post("/logout", adminlogout);
 router.post("/login", proceedingadmin);
 
 module.exports = router;
