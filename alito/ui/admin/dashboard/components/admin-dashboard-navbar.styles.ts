88e508e79bfb0af4fff9f31143675f2abbe8ea7f
Ali-Hassan2 <alihassan26032004@gmail.com>
2025-08-23 19:30:01 +0500

    Working on Admin Dashboard

--- alito/ui/admin/dashboard/components/admin-dashboard-navbar.styles.ts
+++ alito/ui/admin/dashboard/components/admin-dashboard-navbar.styles.ts
@@ -4,12 +4,18 @@ const NavbarWrapper = styled.div`
   width: 100vw;
   height: 50px;
   border: 3px solid red;
+  display: flex;
+  
 `;
 const Left = styled.div`
   width: 50%;
   height: 100%;
   border: 1px solid blue;
 `;
-const Right = styled.div``;
+const Right = styled.div`
+  width: 50%;
+  height: 100%;
+  border: 2px solid white;
+`;
 
 export { NavbarWrapper, Left, Right };
