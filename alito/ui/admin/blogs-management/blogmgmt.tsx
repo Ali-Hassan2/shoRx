84fcf79570ad71248d8075f06f458cb281c8780b
Ali-Hassan2 <alihassan26032004@gmail.com>
2025-08-25 21:07:31 +0500

    Working on admindashboard

---
+++ alito/ui/admin/blogs-management/blogmgmt.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const BlogsMgmt = () => {
+  return <div></div>;
+};
+
+export { BlogsMgmt };
--- alito/ui/index.ts
+++ alito/ui/index.ts
@@ -1,5 +1,8 @@
+import { BlogsMgmt } from "./admin/blogs-management/blogmgmt";
+
 export { Landing } from "./landing/landing";
 export { Blogs } from "./blogs/blogs";
 export { SignUp } from "./admin/signup//signup";
 export { Login } from "./admin/login/login";
 export { Dashboard } from "./admin/dashboard/dashboard";
+export { BlogsMgmt } from "./admin/blogs-management/blogmgmt";
