72584a0b2001d85ae961d56cf33c553e0ee67c38
Ali-Hassan2 <alihassan26032004@gmail.com>
2025-08-25 21:10:06 +0500

    Working on admindashboard

--- alito/pages/admin/dashboard/blog_mgmt/index.tsx
+++ alito/pages/admin/dashboard/blog_mgmt/index.tsx
@@ -1,7 +1,12 @@
+import { BlogsMgmt } from "@/ui";
 import React from "react";
 
 const index = () => {
-  return <div>Hello g this is the admin here</div>;
+  return (
+    <>
+      <BlogsMgmt />
+    </>
+  );
 };
 
 export default index;
--- alito/ui/admin/blogs-management/blogmgmt.tsx
+++ alito/ui/admin/blogs-management/blogmgmt.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const BlogsMgmt = () => {
-  return <div></div>;
+  return <div>This is blogs mgmt</div>;
 };
 
 export { BlogsMgmt };
