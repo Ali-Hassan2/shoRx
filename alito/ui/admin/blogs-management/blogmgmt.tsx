2bca38cbd3083c31bbffbc62388f4bac81823e3c
Ali-Hassan2 <alihassan26032004@gmail.com>
2025-08-25 21:17:17 +0500

    Working on admindashboard

--- alito/ui/admin/blogs-management/blogmgmt.tsx
+++ alito/ui/admin/blogs-management/blogmgmt.tsx
@@ -1,4 +1,12 @@
+import { ROUTES } from "@/lib";
 import React from "react";
+
+const menuItems = [
+  { label: "Home", path: ROUTES.ADMIN.DASHBOARD },
+  { label: "Blogmgmt", path: ROUTES.ADMIN.BLOGMGMT },
+  { label: "Adminmgmt", path: ROUTES.ADMIN.ADMINMGMT },
+  { labe: "Messages", path: ROUTES.ADMIN.MESSAGES },
+];
 
 const BlogsMgmt = () => {
   return <div>This is blogs mgmt</div>;
